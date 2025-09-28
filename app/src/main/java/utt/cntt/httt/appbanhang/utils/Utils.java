f861589b68842e50ff8aa621a356dabca55e1f2a
Congvinh2005 <vanxuank69@gmail.com>
2025-09-28 23:08:16 +0700

    Sữa lỗi tràn chữ và icon góc sản phẩm

--- app/src/main/java/utt/cntt/httt/appbanhang/utils/Utils.java
+++ app/src/main/java/utt/cntt/httt/appbanhang/utils/Utils.java
@@ -1,5 +1,5 @@
 package utt.cntt.httt.appbanhang.utils;
 
 public class Utils {
-    public static final String BASE_URL = "http://10.0.2.2:8888/banhang/";
+    public static final String BASE_URL = "http://192.168.65.103/banhang/";
 }
